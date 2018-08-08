d72c280e4dfcb0252f5b9f0402036e627c50b548
Eli Doran <eli@elidoran.com>
2018-08-07 21:16:36 -0400

    remove semi-colons

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,25 +1,25 @@
-var benchmarked, format, pad, chalk, bold, dim, grey, ask;
+var benchmarked, format, pad, chalk, bold, dim, grey, ask
 
-benchmarked = require('benchmarked');
-format      = require('comma-number');
-pad         = require('pad');
-chalk       = require('chalk');
+benchmarked = require('benchmarked')
+format      = require('comma-number')
+pad         = require('pad')
+chalk       = require('chalk')
 
-bold  = chalk.bold;
-dim   = chalk.dim;
-grey  = chalk.grey;
+bold  = chalk.bold
+dim   = chalk.dim
+grey  = chalk.grey
 
 ask = require('readline').createInterface({
   input : process.stdin,
   output: process.stdout
-});
+})
 
 console.log('@flatten/array benchmark (' + process.pid + ' ' +
-  (process.execArgv.indexOf('--turbo') > -1 ? '- turbo' : '') + ')');
+  (process.execArgv.indexOf('--turbo') > -1 ? '- turbo' : '') + ')')
 
 ask.question('Begin benchmark? (y/N)  ', function(answer) {
 
-  ask.close();
+  ask.close()
 
   if ((answer != null) && (answer[0] === 'y' || answer[0] === 'Y')) {
 
@@ -29,28 +29,28 @@ ask.question('Begin benchmark? (y/N)  ', function(answer) {
       code    : 'implementations/*.js',
 
       format  : function(bm) {
-        var ess, name, ops, rme, runs, size;
+        var ess, name, ops, rme, runs, size
 
-        name = bold(pad(18, bm.name));
-        ops  = pad(10, format(bm.hz.toFixed(bm.hz < 100 ? 2 : 0)));
-        rme  = '\xb1' + bm.stats.rme.toFixed(2).slice(0, 4) + '%';
-        size = pad(2, bm.stats.sample.length);
-        ess  = bm.stats.sample.length === 1 ? ' ' : 's';
-        runs = grey('run' + ess + ' sampled)');
+        name = bold(pad(18, bm.name))
+        ops  = pad(10, format(bm.hz.toFixed(bm.hz < 100 ? 2 : 0)))
+        rme  = '\xb1' + bm.stats.rme.toFixed(2).slice(0, 4) + '%'
+        size = pad(2, bm.stats.sample.length)
+        ess  = bm.stats.sample.length === 1 ? ' ' : 's'
+        runs = grey('run' + ess + ' sampled)')
 
         return name + ' ' + (dim('x')) + ' ' + ops + ' ' + (grey('ops/sec')) +
-          ' ' + rme + ' ' + (grey('(')) + size + ' ' + runs;
+          ' ' + rme + ' ' + (grey('(')) + size + ' ' + runs
       },
 
       renameKey: function(file) {
-        return (file.stem === '_this_library') ? '@flatten/array' : file.stem;
+        return (file.stem === '_this_library') ? '@flatten/array' : file.stem
       }
-    }).run();
+    }).run()
 
   }
 
   else {
-    console.log('quitting');
+    console.log('quitting')
   }
 
-});
+})
